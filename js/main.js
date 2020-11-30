7849ddb1ee9928fd4bf36f333f4bbb22c0fe52e8
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-30 17:39:56 +0100

    Header: Main-menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('ciao js');
+/*
+* Avada Food
+*/
+const app = new Vue({
+  el:'#app',
+  data: {
+    mainNav: [
+      'Home',
+      'Recipes',
+      'Places',
+      'Blog',
+      'About',
+      'Contact'
+    ],
+  },
+  methods: {
+
+  }, // fine methods
+});
