c9bc69255c5553fe6cc93cfe12412bd6122ab09e
cherubina-pantano <cherubina.pantano@gmail.com>
2020-12-01 11:27:45 +0100

    Section1: Foodie Journal

--- js/main.js
+++ js/main.js
@@ -12,6 +12,29 @@ const app = new Vue({
       'About',
       'Contact'
     ],
+    lastBlogPosts: [
+      {
+        path: './Images_Food/single-post-img3-200x132.jpg',
+        title: 'Food Corner: Top Japanese Restaurants for Sushi',
+        author: 'admin',
+        data: 'March 25th, 2019',
+        link:'#'
+      },
+      {
+        path: './Images_Food/fi-roundup-200x132.jpg',
+        title: 'Roundup: My New Favourite Recipes For Healthy Living',
+        author: 'admin',
+        data: 'March 25th, 2019',
+        link:'#'
+      },
+      {
+        path: './Images_Food/fi-toasts-200x132.jpg',
+        title: 'Why These Toasts with Tea are My New Favorite',
+        author: 'admin',
+        data: 'March 25th, 2019',
+        link:'#'
+      },
+    ],
   },
   methods: {
 
